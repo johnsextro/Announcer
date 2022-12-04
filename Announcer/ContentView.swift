52f69004386d115d63fed6b5c0b46c718edc6fb2
John Sextro <john.sextro@gmail.com>
2022-12-03 20:28:11 -0600

    Refactored to create table in a variable in the hopes of being able to call parameters and methods on those variables.

--- Announcer/ContentView.swift
+++ Announcer/ContentView.swift
@@ -31,18 +31,19 @@ struct ContentView: View {
         Person(givenName: "Tom", familyName: "Clark", jerseyNumber: "20", personalFouls: 0),
         Person(givenName: "Gita", familyName: "Kumar", jerseyNumber: "25", personalFouls: 0)
     ]
-    @State private var visitingTeam = [
+    @State private var guestTeam = [
         Person(givenName: "Kate", familyName: "Rolfes", jerseyNumber: "23", personalFouls: 0),
         Person(givenName: "Emily", familyName: "Wilson", jerseyNumber: "35", personalFouls: 0),
         Person(givenName: "Julie", familyName: "Baudendistel", jerseyNumber: "15", personalFouls: 0),
         Person(givenName: "Claire", familyName: "Williams", jerseyNumber: "2", personalFouls: 0)
     ]
     
-    @State private var sortOrder = [KeyPathComparator(\Person.jerseyNumber)]
+    @State private var homeSortOrder = [KeyPathComparator(\Person.jerseyNumber)]
+    @State private var guestSortOrder = [KeyPathComparator(\Person.jerseyNumber)]
     @State private var selection: Person.ID?
     
-    fileprivate func homeTable(players: inout [Person]) -> some View {
-        return Table(players, selection: $selection, sortOrder: $sortOrder) {
+    fileprivate func createPlayerTable(players: inout [Person], sortOrder: Binding<[KeyPathComparator<ContentView.Person>]>) -> some View {
+        return Table(players, selection: $selection, sortOrder: sortOrder) {
             TableColumn("##", value:\.jerseyNumber).width(45)
             TableColumn("Name", value: \.fullName)
             TableColumn("PF") { players in
@@ -53,35 +54,42 @@ struct ContentView: View {
     }
     
     var body: some View {
+        var homeTable = Table(homeTeam, selection: $selection, sortOrder: $homeSortOrder) {
+            TableColumn("##", value:\.jerseyNumber).width(45)
+            TableColumn("Name", value: \.fullName)
+            TableColumn("PF") { homeTeam in
+                Text(String(homeTeam.personalFouls))
+            }.width(35)
+        }.onChange(of: homeSortOrder) { newOrder in
+            homeTeam.sort(using: newOrder)
+        }
+        
+        var guestTable = Table(guestTeam, selection: $selection, sortOrder: $guestSortOrder) {
+            TableColumn("##", value:\.jerseyNumber).width(45)
+            TableColumn("Name", value: \.fullName)
+            TableColumn("PF") { visitingTeam in
+                Text(String(visitingTeam.personalFouls))
+            }.width(35)
+        }.onChange(of: guestSortOrder) { newOrder in
+            guestTeam.sort(using: newOrder)
+        }
+        
             VStack() {
                 HStack() {
                     Text("Home")
                     Button("Foul") {
                         if let unwrapped = selection {
-                            for index in 0..<visitingTeam.count {
-                                if visitingTeam[index].id == unwrapped {
-                                    visitingTeam[index].personalFouls+=1
+                            for index in 0..<guestTeam.count {
+                                if guestTeam[index].id == unwrapped {
+                                    guestTeam[index].personalFouls+=1
                                 }
                             }
                         }
                     }
                 }
                 HStack (alignment: .bottom, spacing: 10){
-                    
-                    homeTable(players: &homeTeam).onChange(of: sortOrder) { newOrder in
-                        homeTeam.sort(using: newOrder)
-                    }
-                    
-                    Table(visitingTeam, selection: $selection, sortOrder: $sortOrder) {
-                        TableColumn("##", value:\.jerseyNumber).width(45)
-                        TableColumn("Name", value: \.fullName)
-                        TableColumn("PF") { visitingTeam in
-                            Text(String(visitingTeam.personalFouls))
-                        }.width(35)
-                    }
-                    .onChange(of: sortOrder) { newOrder in
-                        visitingTeam.sort(using: newOrder)
-                    }
+                    homeTable
+                    guestTable
                 }
             }
     }
